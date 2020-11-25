95c78abb2bea9f7e628698c9c0035cd3b45b3114
ArtemEvgl <Usbhawk5@gmail.com>
2020-11-25 08:31:54 +0300

    Update lesson 7

--- Lesson07.3/MainWindow.xaml.cs
+++ Lesson07.3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -22,6 +23,7 @@ namespace Lesson07._3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Assembly assembly = null;
         public ObservableCollection<BoolStringClass> TheList { get; set; }
         public MainWindow()
         {
@@ -42,12 +44,73 @@ namespace Lesson07._3
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    assembly = Assembly.LoadFile(openFileDialog.FileName);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (assembly == null)
+            {
+                MainText.Text = "Файл не выбран";
+                return;
+            }
+            MainText.Text = "";
 
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types)
+            {
+                MainText.Text += $"Тип: {type} {Environment.NewLine} {Environment.NewLine}";
+                object[] attributes = type.GetCustomAttributes(false);
+                if (attributes.Length > 0 && (bool)checkBoxAtrType.IsChecked)
+                {
+                    MainText.Text += "Атрибуты типа";
+                    foreach (var attr in attributes)
+                    {
+                        MainText.Text += attr + Environment.NewLine;
+                    }
+                }
+
+                var members = type.GetMembers();
+                if (members != null)
+                {
+                    for (int i = 0; i < TheList.Count; i++)
+                    {
+                        if (TheList[i].IsSelected)
+                        {
+                            object element = Enum.Parse(typeof(MemberTypes), TheList[i].TheText);
+                            MemberTypes memberType = (MemberTypes)element;
+                            foreach (var member in members)
+                            {
+                                if (member.MemberType == memberType)
+                                {
+                                    string methStr = member.MemberType.ToString().ToUpper() + " " + member.Name + "\n";
+                                    MainText.Text += methStr + Environment.NewLine;
+                                    object[] attributesMeth = member.GetCustomAttributes(false);
+                                    if (attributesMeth.Length > 0 && (bool)checkBoxAtrMember.IsChecked)
+                                    {
+                                        MainText.Text += "Атрибуты метода: ";
+                                        foreach (var attr in attributesMeth)
+                                        {
+                                            MainText.Text += attr + Environment.NewLine;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                MainText.Text += Environment.NewLine;
+            }
         }
 
         public class BoolStringClass
