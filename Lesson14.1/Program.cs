5b917ea57225669123b7c3941a9119e5be00fc90
ArtemEvgl <Usbhawk5@gmail.com>
2021-01-01 23:04:12 +0300

    Homework lesson14

---
+++ Lesson14.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[1000000];
+            Parallel.For(0, numbers.Length, i => numbers[i] = random.Next());
+            var unevenNumbers = numbers.AsParallel().Where(i => i % 2 != 0);
+            unevenNumbers.ForAll(i => Console.WriteLine(i));
+        }
+    }
+}
---
+++ Lesson14.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14._2
+{
+    class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("Method 1 is started");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Counter method's 1 = {i}");
+            }
+            Console.WriteLine("Method 1 is finished");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method 2 is started");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Counter method's 2 = {i}");
+            }
+            Console.WriteLine("Method 2 is finished");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread is started");
+            Task.Factory.StartNew(() => Parallel.Invoke(Method1, Method2));
+            Console.WriteLine("Main thread is finished");
+            Console.ReadKey();
+        }
+    }
+}
