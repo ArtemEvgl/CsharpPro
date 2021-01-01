21e0601a08b0d149db966c76625571beaef0c07e
ArtemEvgl <Usbhawk5@gmail.com>
2021-01-01 23:06:11 +0300

    Homework lesson15

---
+++ Lesson15.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson15._1
+{
+    class Program
+    {
+        
+        
+        public static int Operation(object state)
+        {
+            int result = (int)state;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(++result);
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            return result; 
+        }
+        
+        public static async Task OperationAsync(int number)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            number = await Task<int>.Factory.StartNew(Operation, number);
+            number = await Task.Factory.StartNew(Operation, number);
+            await Task.Factory.StartNew(Operation, number);
+            Console.WriteLine($"The end {Thread.CurrentThread.ManagedThreadId}");
+        }
+        
+        static void Main(string[] args)
+        {
+            Task task = OperationAsync(0);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Lesson15.2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+
+namespace Lesson15._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Forms.Timer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 5000;
+            timer.Tick += timer_Tick;
+        }
+
+        async void timer_Tick(object sender, EventArgs e)
+        {
+            TextBox.Text += await GetDataAsync();
+            TextBox.ScrollToEnd();
+        }
+
+        private Task<string> GetDataAsync()
+        {
+            return Task.Run(() => { return "data received" + Environment.NewLine; });
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            ConnectButton.IsEnabled = false;
+            TextBox.Text += await ConnectDbAsync();
+            DisconnectButton.IsEnabled = true;
+        }
+
+        private Task<string> ConnectDbAsync()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return "Connected" + Environment.NewLine;
+            });
+        }
+
+        private async void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            DisconnectButton.IsEnabled = false;
+            TextBox.Text += await DisconnectDbAsync();
+            ConnectButton.IsEnabled = true;
+        }
+
+        private async Task<string> DisconnectDbAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return "Disconnected" + Environment.NewLine;
+            });
+        }
+    }
+}
